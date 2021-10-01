0ceffe3d0cb45f2d9afce452a2a1672eaf14131a
Abhishek Mohabe <abhishek.m28@gmail.com>
2021-09-30 23:38:02 -0700

    Added React and Spring Boot dependencies
    Added React and Spring Boot dependencies

---
+++ src/main/java/io/soccerapp/premiersoccerleaguecup/PremierSoccerLeagueCupApplication.java
@@ -0,0 +1,13 @@
+package io.soccerapp.premiersoccerleaguecup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierSoccerLeagueCupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierSoccerLeagueCupApplication.class, args);
+	}
+
+}
