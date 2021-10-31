5db1417b556d38cd7f8b14017cc17168c8eca38e
sbolla3 <sbolla3@asu.edu>
2021-10-31 16:43:18 -0700

    Updated Webpage
    Added more details in the webpage and tested for large data

--- src/frontend/src/components/GroupDetails.js
+++ src/frontend/src/components/GroupDetails.js
@@ -1,11 +1,13 @@
 import React from "react";
+import match from "match.jpg";
 
 const Details=()=>{
-    return <div>
-        <table>
-            <thead>
+    return <div style={{  
+           backgroundImage: "url(" + match + ")"
+           }}>
+        <table style={{marginLeft:460,width:'500px' }}>
+            <thead style={{backgroundColor:'grey' }}>
                 <tr>
-                    <th>Date</th>
                     <th>Group1</th>
                     <th>Group2</th>
                     <th>Venue</th>
@@ -31,8 +33,20 @@ const Details=()=>{
                     <td>Ground3</td>
                     <td>10 Feb 2021</td>
                 </tr>
+                <tr>
+                    <td>USA</td>
+                    <td>Germany</td>
+                    <td>Ground4</td>
+                    <td>10 Feb 2021</td>
+                </tr>
+                <tr>
+                    <td>USA</td>
+                    <td>Canada</td>
+                    <td>Ground5</td>
+                    <td>11 August 2022</td>
+                </tr>
             </tbody>    
             </table>
         </div>;
 }
-export default Referee;
+export default Details;
