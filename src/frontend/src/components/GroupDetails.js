78ccc9b6078372e1727fbc9cfdb4d37a882c05b0
sbolla3 <sbolla3@asu.edu>
2021-10-31 13:57:03 -0700

    Connection to database and UI for the data from database
    UI for the data from database

---
+++ src/frontend/src/components/GroupDetails.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Details=()=>{
+    return <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Group1</th>
+                    <th>Group2</th>
+                    <th>Venue</th>
+                    <th>Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>USA</td>
+                    <td>England</td>
+                    <td>Ground1</td>
+                    <td>05 October 2021</td>
+                </tr>
+                <tr>
+                    <td>England</td>
+                    <td>Australia</td>
+                    <td>Ground2</td>
+                    <td>06 September 2021</td>
+                </tr>
+                <tr>
+                    <td>Australia</td>
+                    <td>Germany</td>
+                    <td>Ground3</td>
+                    <td>10 Feb 2021</td>
+                </tr>
+            </tbody>    
+            </table>
+        </div>;
+}
+export default Referee;
