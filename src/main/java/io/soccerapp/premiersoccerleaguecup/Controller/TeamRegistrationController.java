6a00c3efd358b687bbdf370c7fb869a3a67e63eb
gautham <gautham.krishna05@gmail.com>
2021-10-31 16:49:02 -0700

    Added controller, Repo files

---
+++ src/main/java/io/soccerapp/premiersoccerleaguecup/Controller/TeamRegistrationController.java
@@ -0,0 +1,53 @@
+package io.soccerapp.premiersoccerleaguecup.Controller;
+
+import java.util.Iterator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import io.soccerapp.premiersoccerleaguecup.Java.TeamRegistration;
+import io.soccerapp.premiersoccerleaguecup.Repository.TeamRegistrationRepository;
+
+@RestController
+@RequestMapping("/v1")
+public class TeamRegistrationController {
+
+	@Autowired
+	private TeamRegistrationRepository userRepository;
+
+	@CrossOrigin(origins = "*")
+	@PostMapping("/signup")
+	public String saveUser(@RequestBody TeamRegistration data) {
+		Iterable<TeamRegistration> userlist = userRepository.findAll();
+		Iterator<TeamRegistration> it = userlist.iterator();
+		while (it.hasNext()) {
+			TeamRegistration logindetail = it.next();
+			if (logindetail.getEmail() != null && logindetail.getEmail().equals(data.getEmail()))
+				return "User is already already registered! Please try sign in!";
+		}
+		this.userRepository.save(data);
+		return "User registered " + data.toString();
+	}
+
+	@CrossOrigin(origins = "*")
+	@PostMapping("/login")
+	public String getUser(@RequestBody TeamRegistration data) {
+		Iterable<TeamRegistration> userlist = userRepository.findAll();
+
+		Iterator<TeamRegistration> it = userlist.iterator();
+		while (it.hasNext()) {
+			TeamRegistration logindetail = it.next();
+			if (logindetail.getEmail() != null && logindetail.getEmail().equals(data.getEmail()))
+				return "User logged in";
+		}
+
+		return "User not regsitered";
+
+	}
+
+}
