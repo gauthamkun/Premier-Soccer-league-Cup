ef31db89bae6f4ac17030297efa630bc0f461fa2
unknown <apoorvagiliyal@gmail.com>
2021-10-31 23:11:31 -0700

    Adding Login Folder

---
+++ src/frontend/src/Login/App.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import "./App.css";
+import UserStore from "./stores/UserStore";
+import React from "react";
+
+class App extends React.Component() {
+  render() {
+    return <div className="App"></div>;
+  }
+}
+export default App;
