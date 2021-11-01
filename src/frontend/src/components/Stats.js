1f400578d7becd3554de4762fc56dbc5f46bca66
Abhishek Mohabe <abhishek.m28@gmail.com>
2021-10-31 19:44:21 -0700

    API UI view updated

--- src/frontend/src/components/Stats.js
+++ src/frontend/src/components/Stats.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { Component } from 'react';
 
-function Stats() {
-  return (
-    <>
-      <table style={{width:'500px' }}>
-            <thead style={{backgroundColor:'orange' }}>
-                <tr>
+class Stats extends Component {
+  state = {
+    isLoading: true,
+    groups: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/Stats/api');
+    const body = await response.json();
+    this.setState({ groups: body, isLoading: false });
+  }
+
+  render() {
+    const {groups, isLoading} = this.state;
+   
+    return (
+      <div class="container">
+          <img src="images/pslcup.png" alt={"images/4.jpg"} width="150px" height="150px"/>
+          <div class="container">
+            <h2>Tournament Statistics</h2>
+            <table style={{width:'500px' }}>
+                <thead style={{backgroundColor:'orange' }}>
+                  <tr>
                     <th>Position</th>
                     <th>Team Name</th>
                     <th>Points</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
                     <td>Position</td>
                     <td>Arizona State University </td>
                     <td>92</td>
-                </tr>
-              </tbody>
-              </table>
-    </>
-  );
+                  </tr>
+                </tbody>
+            </table>
+            {groups.map(group =>
+              <div key={group.id}>
+                {group.name}
+              </div>
+            )}
+          </div>
+      </div>
+    );
+  }
 }
-
 export default Stats;
