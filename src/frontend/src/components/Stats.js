1ce0aaae1a945bfbc8ab2db8395cc9dae86d3bb7
Abhishek Mohabe <abhishek.m28@gmail.com>
2021-10-31 18:12:14 -0700

    Task#62 complete - generate views of stats table

--- src/frontend/src/components/Stats.js
+++ src/frontend/src/components/Stats.js
@@ -3,7 +3,22 @@ import React from "react";
 function Stats() {
   return (
     <>
-      <h2>Tournament Statitistics - Coming Soon!!</h2>
+      <table style={{width:'500px' }}>
+            <thead style={{backgroundColor:'orange' }}>
+                <tr>
+                    <th>Position</th>
+                    <th>Team Name</th>
+                    <th>Points</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Position</td>
+                    <td>Arizona State University </td>
+                    <td>92</td>
+                </tr>
+              </tbody>
+              </table>
     </>
   );
 }
--- src/main/java/io/soccerapp/premiersoccerleaguecup/Repository/TeamRegistrationRepository.java
+++ src/main/java/io/soccerapp/premiersoccerleaguecup/Repository/TeamRegistrationRepository.java
@@ -1,4 +1,4 @@
-package io.soccerapp.premiersoccerleaguecup.Repository;
+package io.soccerapp.premiersoccerleaguecup.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
