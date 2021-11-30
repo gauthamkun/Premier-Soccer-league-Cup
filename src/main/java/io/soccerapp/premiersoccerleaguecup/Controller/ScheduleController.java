4007264a1297a852dd5b05c9cd81c4b65df4ae3a
sbolla3 <sbolla3@asu.edu>
2021-11-29 21:31:23 -0700

    Dynamic Schedule Page

---
+++ src/main/java/io/soccerapp/premiersoccerleaguecup/Controller/ScheduleController.java
@@ -0,0 +1,37 @@
+package io.soccerapp.premiersoccerleaguecup.Controller;
+
+import java.util.Iterator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.soccerapp.premiersoccerleaguecup.Java.Scheduling;
+import io.soccerapp.premiersoccerleaguecup.Repository.ScheduleRepository;
+
+@RestController
+@RequestMapping("/v1")
+public class ScheduleController {
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+
+    @CrossOrigin(origins = "*")
+    @PostMapping("/Scheduling")
+    public String saveUser(@RequestBody Scheduling data) {
+        Iterable<Scheduling> scheduledata = scheduleRepository.findAll();
+        Iterator<Scheduling> it = scheduledata.iterator();
+        while (it.hasNext()) {
+            Scheduling schedule = it.next();
+
+            if (schedule.getground().equals(data.getground()))
+                return "The Selected ground is already filled. Please choose a different ground";
+        }
+        this.scheduleRepository.save(data);
+        return "Scheduled match" + data.toString();
+
+    }
+
+}
