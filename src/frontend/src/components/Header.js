4d5a37f56f57295e6824e2189273dc80116ce7cc
unknown <apoorvagiliyal@gmail.com>
2021-11-29 21:48:56 -0700

    Header file has been modified

--- src/frontend/src/components/Header.js
+++ src/frontend/src/components/Header.js
@@ -1,52 +1,130 @@
-import React from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import AboutUs from './AboutUs'
-import TeamRegisteration from './TeamRegisteration'
-import Stats from './Stats';
-import Scheduling from './Scheduling';
-import Home from './Home'
-import Referee from './Referee';
-import Map from './Map';
-import Rules from './Rules';
-import Form from './Form';
+import AboutUs from "./AboutUs";
+import TeamRegisteration from "./TeamRegisteration";
+import Stats from "./Stats";
+import Scheduling from "./Scheduling";
+import Home from "./Home";
+import Referee from "./Referee";
+import Map from "./Map";
+import Rules from "./Rules";
+import Form from "./Form";
+import Login from "./Login";
+import RefereeRegistration from "./RefereeRegistration";
+import TeamRegistrationView from "./TeamRegistrationView";
+import SchedulingView from "./SchedulingView";
 
 function Header() {
-
+  const userRole = localStorage.getItem("role");
   return (
     <Router>
-        <header>
-            <nav>
-                <ul>
-                    <li> <Link to="/">Home</Link></li>
-                    <li> <Link to="/about">About Us</Link></li>
-                    <li> <Link to="/Rules">Tournament Rules</Link></li>
-                    <li> <Link to="/TeamRegistration">Team Registration</Link></li>
-                    <li> <Link to="/Stats">Statistics</Link></li>
-                    <li> <Link to="/Scheduling">Match Scheduling</Link></li>
-                    <li> <Link to="/Referee">Referee section</Link></li>
-                    <li> <Link to="/Map">Maps</Link></li>
-                    <li> <Link to="/Form">Login / Sign Up</Link></li>
-                </ul>
-            </nav>
-        </header>
-        <Switch>
-            <Route path="/about"> <AboutUs /></Route>
-            <Route path="/TeamRegistration"><TeamRegisteration /></Route>
-            <Route path="/Rules"><Rules /></Route>
-            <Route path="/stats"><Stats /></Route>
-            <Route path="/Scheduling"><Scheduling /></Route>
-            <Route path="/Referee"><Referee /></Route>
-            <Route path="/Map"><Map /></Route>
-            <Route path="/Form"><Form /></Route>
-            <Route path="/"><Home /> </Route>
-        </Switch>
-      </Router>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              {" "}
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="/Rules">Tournament Rules</Link>
+            </li>
+            {userRole == 1 || userRole == 3 ? (
+              <li>
+                {" "}
+                <Link to="/TeamRegistration">Team Registration</Link>
+              </li>
+            ) : (
+              <li>
+                {" "}
+                <Link to="/TeamRegistrationView">Team Registration</Link>
+              </li>
+            )}
+            <li>
+              {" "}
+              <Link to="/Stats">Statistics</Link>
+            </li>
+            {userRole == 1 || userRole == 5 ? (
+              <li>
+                {" "}
+                <Link to="/Scheduling">Match Scheduling</Link>
+              </li>
+            ) : (
+              <li>
+                {" "}
+                <Link to="/SchedulingView">Match Scheduling</Link>
+              </li>
+            )}
+            {userRole == 1 || userRole == 4 ? (
+              <li>
+                {" "}
+                <Link to="/Referee">Referee section</Link>
+              </li>
+            ) : (
+              <li>
+                {" "}
+                <Link to="/RefereeRegistration">Referee section</Link>
+              </li>
+            )}
+            <li>
+              {" "}
+              <Link to="/Map">Maps</Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="/Login">Login / Sign Up</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Switch>
+        <Route path="/about">
+          {" "}
+          <AboutUs />
+        </Route>
+        <Route path="/TeamRegistration">
+          <TeamRegisteration />
+        </Route>
+        <Route path="/Rules">
+          <Rules />
+        </Route>
+        <Route path="/stats">
+          <Stats />
+        </Route>
+        <Route path="/Scheduling">
+          <Scheduling />
+        </Route>
+        <Route path="/Referee">
+          <Referee />
+        </Route>
+        <Route path="/Map">
+          <Map />
+        </Route>
+        <Route path="/Login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Form />
+        </Route>
+        <Route path="/RefereeRegistration">
+          <RefereeRegistration />
+        </Route>
+        <Route path="/TeamRegistrationView">
+          <TeamRegistrationView />
+        </Route>
+        <Route path="/SchedulingView">
+          <SchedulingView />
+        </Route>
+        <Route path="/">
+          <Home />{" "}
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
