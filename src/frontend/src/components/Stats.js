7358a1932a1d1051c5e2cc404e7118d1f0532736
Abhishek Mohabe <abhishek.m28@gmail.com>
2021-11-29 19:28:14 -0700

    Included the statistics JS file. US#95 complete

--- src/frontend/src/components/Stats.js
+++ src/frontend/src/components/Stats.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import Table from "./components/Table";
 
-function Stats() {
+class Stats extends Component{
+  render(){
+    var current = new Date();
+    var expiry = new Date("December 01, 2021 17:39:00")
   return (
-    <>
-      <h2>Tournament Statitistics - Coming Soon!!</h2>
-    </>
-  );
+    
+    <div>
+    {
+    current.getTime() < expiry.getTime()?<div>
+            <Router>
+            <Route exact path ="/stats" component={Table} />
+            </Router>
+            </div>:<p>Application process in not available</p> }
+    </div>
+          );
+  }
 }
 
 export default Stats;
